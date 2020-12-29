2bb095c5e2659a7b1a7e0ed09eaa2f6bea049fef
Nico <nicolas.judalet@gmail.com>
2020-12-30 00:52:18 +0100

    feat: configure Tailwind css and add homepage with title

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="h-screen w-screen flex justify-center">
+    <h1 className="w-full h-16 text-4xl text-blue-400 font-sans p-4">
+      <span aria-label="book" className="mr-2">
+        📘
+      </span>
+      Le voyageur immobile
+    </h1>
+  </div>
+);
+
+export default Home;
